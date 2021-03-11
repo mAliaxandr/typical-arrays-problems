0a3c23cc883415fd07832ca6c9399de1c65a9d9e
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-11 17:27:07 +0300

    feat: implement function min

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 
 exports.min = function min (array) {
+    if (array) {
+        if (array.length == 0) {
+            return 0
+        }
+        const arr = array;
+    let result = arr[0];
+    arr.forEach( i => {
+        if (i < result) {
+            result = i;
+        }
+    })
+  return result;
+    }
+
+
+    return 0;
     
-  return 0;
 }
 
 exports.max = function max (array) {
+    console.log('max ---', array);
   return 0;
 }
 
 exports.avg = function avg (array) {
+    console.log('avg ---', array);
   return 0;
 }
