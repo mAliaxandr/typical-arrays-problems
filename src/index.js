f6f9d73daba47641a13f22a3559f066ca78b00c0
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-11 19:43:28 +0300

    feat: implement function avg

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-    console.log('max ---', array);
     if (array) {
         if (array.length == 0) {
             return 0
@@ -32,10 +31,20 @@ exports.max = function max (array) {
          return result;
     }
     return 0;  
-  return 0;
 }
 
 exports.avg = function avg (array) {
     console.log('avg ---', array);
-  return 0;
+    if (array) {
+        if (array.length == 0) {
+            return 0
+        }
+        const arr = array;
+        let result = 0;
+        arr.forEach( i => {
+            result = result + i;
+        })
+         return result / arr.length;
+    }
+    return 0;  
 }
