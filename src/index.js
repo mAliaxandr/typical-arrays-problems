ccf237a3f8c85af1b461e3d834304466304915f7
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-11 17:29:49 +0300

    feat: implement function max

--- src/index.js
+++ src/index.js
@@ -5,22 +5,33 @@ exports.min = function min (array) {
             return 0
         }
         const arr = array;
-    let result = arr[0];
-    arr.forEach( i => {
-        if (i < result) {
-            result = i;
-        }
-    })
-  return result;
+        let result = arr[0];
+        arr.forEach( i => {
+            if (i < result) {
+                result = i;
+            }
+        })
+         return result;
     }
-
-
-    return 0;
-    
+    return 0;  
 }
 
 exports.max = function max (array) {
     console.log('max ---', array);
+    if (array) {
+        if (array.length == 0) {
+            return 0
+        }
+        const arr = array;
+        let result = arr[0];
+        arr.forEach( i => {
+            if (i > result) {
+                result = i;
+            }
+        })
+         return result;
+    }
+    return 0;  
   return 0;
 }
 
